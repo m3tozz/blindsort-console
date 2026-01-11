3d9a294a2a5e3aeed0b5ce183ae4a1ef1f74a5b7
Metin Zühre <79897762+m3tozz@users.noreply.github.com>
2026-01-11 16:00:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blindsort_cmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("How many numbers should be entered: ");
+                Console.ResetColor();
+
+                int adet = Convert.ToInt32(Console.ReadLine());
+
+                int[] kisim = new int[adet];
+                Random rnd = new Random();
+
+                for (int i = 0; i < adet; i++)
+                {
+                    int sayi = rnd.Next(1, 51);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("\n" + sayi + "\n");
+                    Console.ResetColor();
+
+                    Console.Write(sayi + " which position should it be placed in (1-" + adet + "): ");
+                    int sira = Convert.ToInt32(Console.ReadLine());
+
+                    kisim[sira - 1] = sayi;
+
+                    // Diziyi göster
+                    Console.SetCursorPosition(0, adet);
+                    for (int j = 0; j < adet; j++)
+                    {
+                        if (kisim[j] == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+                            Console.Write("- ");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write(kisim[j] + " ");
+                        }
+                        Console.ResetColor();
+                    }
+                    Console.WriteLine();
+                }
+
+                bool lost = false;
+                for (int j = 0; j < adet - 1; j++)
+                {
+                    if (kisim[j] > kisim[j + 1])
+                    {
+                        lost = true;
+                        break;
+                    }
+                }
+
+                if (lost)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nYou lost");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nCongratulations, you won");
+                }
+                Console.ResetColor();
+
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You did not enter a number");
+                Console.ResetColor();
+
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("blindsort-console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("blindsort-console")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("c8f0c16e-ee5d-41f9-80f2-7bd5a8e04d4c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
